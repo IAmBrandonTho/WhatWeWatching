7c5b0b26eaf01d3537ee8a9b0f1833fc2c3cee16
IAmBrandonTho <98342097+IAmBrandonTho@users.noreply.github.com>
2026-02-19 23:11:13 -0500

    Delete public directory

--- public/state.js
+++
@@ -1,8 +0,0 @@
-export const AppState = {
-  role: "viewer",
-  connection: "disconnected",
-  streaming: false,
-  video: { paused: true, hasStream: false, stream: null },
-  peers: [],
-  ui: { peopleOpen: false, qualityOpen: false }
-};
