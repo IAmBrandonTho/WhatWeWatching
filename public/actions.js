d33f382223b1cc982bfab6ab6bb36423de735628
IAmBrandonTho <98342097+IAmBrandonTho@users.noreply.github.com>
2026-02-20 01:38:43 -0500

    Wipe

--- public/actions.js
+++
@@ -1,8 +0,0 @@
-import { reducer } from "./reducer.js";
-import { render } from "./render.js";
-import { AppState } from "./state.js";
-
-export function dispatch(action){
-  reducer(AppState, action);
-  render(AppState, action);
-}
