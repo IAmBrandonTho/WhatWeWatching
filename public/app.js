405b73866f197047e93a749a8a0d8d6ba59189e9
IAmBrandonTho <98342097+IAmBrandonTho@users.noreply.github.com>
2026-02-20 16:51:43 -0500

    ss

--- public/app.js
+++
@@ -1,7 +0,0 @@
-import { startNetworking } from "./networking.js";
-import { dispatch } from "./actions.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  dispatch({ type: "DISCONNECTED" });
-  startNetworking();
-});
